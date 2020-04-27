a26bb356234648063bba6da3adfde52dbc492dc5
savelgmx <savel@gmx.com>
2020-04-27 19:00:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/filmrepositoryapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.filmrepositoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        В этом задании работа с Realm. интерфейс FilmRepository
+        стандартные методы получения, вставки, изменения, удаления элемента (Film).
+                Плюс получения всех фильмов
+        Добавить возможность генерации данных для заполнения БД
+*/
+
+
+    }
+}
