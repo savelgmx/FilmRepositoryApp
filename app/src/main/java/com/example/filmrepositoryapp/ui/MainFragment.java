f8c8775612af43e3c596f197a39a9ac0dba5aeaa
savelgmx <savel@gmx.com>
2020-05-10 16:16:17 +0700

    commit#11 Пишем MainFragment запуск без ошибок но пока без данных Экран для генерации данных для заполнения БД (добавления редактирования БД)

--- app/src/main/java/com/example/filmrepositoryapp/ui/MainFragment.java
+++ app/src/main/java/com/example/filmrepositoryapp/ui/MainFragment.java
@@ -1,16 +1,33 @@
 package com.example.filmrepositoryapp.ui;
 
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.filmrepositoryapp.R;
 
 public class MainFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private RecyclerView mRecyclerView;
     private SwipeRefreshLayout mRefresher;
     private View mErrorView;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_recycler, container, false);
+    }
 
 
     @Override
