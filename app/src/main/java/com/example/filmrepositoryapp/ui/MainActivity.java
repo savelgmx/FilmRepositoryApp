bd2acd7a223fb2f586d8b9f9deaad5d740f634bc
savelgmx <savel@gmx.com>
2020-05-11 11:46:21 +0700

    commit#14 MainActivity extends SingleFragmentActivity FilmsAdapter дописать для заполнения данными запуск без ошибок но пока без данных Экран для генерации данных для заполнения БД (добавления редактирования БД)

--- app/src/main/java/com/example/filmrepositoryapp/ui/MainActivity.java
+++ app/src/main/java/com/example/filmrepositoryapp/ui/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.filmrepositoryapp.ui;
 
-
-
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.example.filmrepositoryapp.R;
 
 
-//TODO SingleFragmentActivity унаследовать
-
-
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
       }
+      @Override
+    protected Fragment getFragment() {
+        return MainFragment.newInstance();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
