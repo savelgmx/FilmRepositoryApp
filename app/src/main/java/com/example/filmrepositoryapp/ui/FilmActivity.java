c3dc5081e2dc7e382091fbb137bd671cf407d7cc
savelgmx <savel@gmx.com>
2020-06-10 18:42:06 +0700

    Устранены NPE Error inflating class android.support.design.widget.CoordinatorLayout TODO сделать заполнение recycler view заполнеие списка

--- app/src/main/java/com/example/filmrepositoryapp/ui/FilmActivity.java
+++ app/src/main/java/com/example/filmrepositoryapp/ui/FilmActivity.java
@@ -1,14 +1,13 @@
 package com.example.filmrepositoryapp.ui;
 
 
-import android.graphics.Color;
+import android.content.DialogInterface;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,6 +21,7 @@ import com.example.filmrepositoryapp.model.RealmManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import butterknife.OnClick;
 import io.realm.Realm;
 /*
         В этом задании работа с Realm. интерфейс FilmRepository
@@ -62,7 +62,6 @@ public class FilmActivity extends AppCompatActivity  implements FilmPresenter.Vi
 
         //get presenter instance
         filmPresenter = fragment.getPresenter();
-
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
@@ -82,9 +81,12 @@ public class FilmActivity extends AppCompatActivity  implements FilmPresenter.Vi
         // changes will be reflected automatically
         recycler.setAdapter(new FilmAdapter(realm.where(Film.class).findAllAsync()));
 
+        if(savedInstanceState == null) {
+            Toast.makeText(this, "R.string.press_to_edit_long_press_remove", Toast.LENGTH_LONG).show();
+        }
 
-
-
+        // bind to presenter
+        filmPresenter.bindView(this);
     }
 
 
@@ -102,17 +104,94 @@ public class FilmActivity extends AppCompatActivity  implements FilmPresenter.Vi
 */
 
     @Override
+    protected void onDestroy() {
+        if(filmPresenter != null) {
+            filmPresenter.unbindView();
+        }
+        if(dialog != null) {
+            dialog.dismiss();
+        }
+        super.onDestroy();
+    }
+
+    @OnClick(R.id.fab)
+    void onFabClicked() {
+        filmPresenter.showAddDialog();
+    }
+
+
+    @Override
     public void showAddFilmDialog() {
+
+        final View content = getLayoutInflater().inflate(R.layout.edit_item, root, false);
+        final DialogContract dialogContract = (DialogContract) content;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(FilmActivity.this);
+        builder.setView(content)
+                .setTitle(getString(R.string.add_film))
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        filmPresenter.saveFilm(dialogContract);
+                        filmPresenter.dismissAddDialog();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        filmPresenter.dismissAddDialog();
+                        dialog.dismiss();
+                    }
+                });
+        dialog = builder.create();
+        dialog.show();
 
     }
 
     @Override
     public void showMissingFilmName() {
+        Toast.makeText(FilmActivity.this,getString(R.string.entry_not_saved),
+                Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void showEditFilmDialog(Film film) {
 
+        if(!film.isValid()) {
+            return;
+        }
+        final View content = getLayoutInflater().inflate(R.layout.edit_item, root, false);
+        final FilmPresenter.ViewContract.DialogContract dialogContract = (FilmPresenter.ViewContract.DialogContract) content;
+        dialogContract.bind(film);
+
+        final long id = film.getId();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(FilmActivity.this);
+        builder.setView(content)
+                .setTitle(getString(R.string.edit_film))
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        filmPresenter.editFilm(dialogContract, id);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
+    @Override
+    public Object getSystemService(String name) {
+        if(name.equals(FilmPresenter.TAG)) {
+            return filmPresenter;
+        }
+        return super.getSystemService(name);
+    }
+
 }
