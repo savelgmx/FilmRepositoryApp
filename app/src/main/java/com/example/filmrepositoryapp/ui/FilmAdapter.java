240f8e80a035d1bcb4a73be343ff6a5ae01ed6a2
savelgmx <savel@gmx.com>
2020-06-12 20:20:11 +0700

    Устранены переписан ВьюХолдер под МВП TODO сделать заполнение recycler view with initial data

--- app/src/main/java/com/example/filmrepositoryapp/ui/FilmAdapter.java
+++ app/src/main/java/com/example/filmrepositoryapp/ui/FilmAdapter.java
@@ -1,81 +1,51 @@
 package com.example.filmrepositoryapp.ui;
 
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
-
 import com.example.filmrepositoryapp.R;
 import com.example.filmrepositoryapp.model.Film;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import io.realm.RealmModel;
 import io.realm.RealmRecyclerViewAdapter;
 import io.realm.RealmResults;
 
 
-public class FilmAdapter extends RealmRecyclerViewAdapter<Film,FilmsHolder> {
+public class FilmAdapter extends RealmRecyclerViewAdapter<Film, FilmsViewHolder> {
 
-    public static OnItemClickListener onItemClickListener;
     @NonNull
-        private final List<Film> mFilms = new ArrayList<>();
-       // private final OnItemClickListener mOnClickListener;
+    private final List<Film> mFilms = new ArrayList<>();
 
-        public FilmAdapter(RealmResults<Film> films) {
-            super(films,true);
-           // mOnClickListener = onClickListener;
-        }
-/*
-    public FilmAdapter(RealmResults<Film> allAsync) {
-        super(allAsync,true);
+    public FilmAdapter(RealmResults<Film> films) {
+        super(films,true);
     }
-*/
+
+    // create new views (invoked by the layout manager)
+    @Override
+    public FilmsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_films, parent, false);
+        return new FilmsViewHolder(view);
+    }
 
     @Override
-        public FilmsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            View view = inflater.inflate(R.layout.list_item_film, parent, false);
-            return new FilmsHolder(view);
-        }
+    public void onBindViewHolder(FilmsViewHolder holder, final int position) {
 
-
-
-    @Override
-        public void onBindViewHolder(FilmsHolder holder, final int position) {
-
-            final Film film = getItem(position);
-            if (film != null){
-              //  holder.bind(film,mOnClickListener);
-                holder.bind(film);
-
-            }
+        final Film film = getItem(position);
+        if (film != null){
+            holder.bind(film);
 
         }
 
-
-        @Override
-        public int getItemCount() {
-            return mFilms.size();
-        }
-
-        public void addData(List<Film> data, boolean isRefreshed) {
-            if (isRefreshed) {
-                mFilms.clear();
-            }
-
-            mFilms.addAll(data);
-            notifyDataSetChanged();
-        }
-
-
-
-    public interface OnItemClickListener {
-            void onItemClick(Film film);
-        }
     }
+
+
+
+}
+
+
+
+
 
 
