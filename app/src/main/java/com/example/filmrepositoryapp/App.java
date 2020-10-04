239dc99a1c88cfaf4d03729d124a0e4461148dd3
savelgmx <savel@gmx.com>
2020-10-04 21:41:28 +0700

    1-st commit in this branch Added App.java

---
+++ app/src/main/java/com/example/filmrepositoryapp/App.java
@@ -0,0 +1,34 @@
+package com.example.filmrepositoryapp;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.widget.Toast;
+
+
+
+import io.realm.Realm;
+import toothpick.Scope;
+import toothpick.Toothpick;
+import toothpick.registries.FactoryRegistryLocator;
+import toothpick.registries.MemberInjectorRegistryLocator;
+
+public class App extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+
+        Toothpick.setConfiguration(Configuration.forProduction().disableReflection());
+        Toothpick.setConfiguration(Configuration.forDevelopment().preventMultipleRootScopes());
+        MemberInjectorRegistryLocator.setRootRegistry(new com.example.alien.course04task02.MemberInjectorRegistry());
+        FactoryRegistryLocator.setRootRegistry(new com.example.alien.course04task02.FactoryRegistry());
+        Scope scope = Toothpick.openScope("Application");
+        scope.installModules(new ApplicationModule(this));
+
+
+
+        Toast.makeText(this, R.string.start_greeting, Toast.LENGTH_LONG).show();
+    }
+}
